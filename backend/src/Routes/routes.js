0b1372c41dda233ad7533e7f06c28572ea788657
Pedro Henrique <pedrook16@gmail.com>
2019-10-08 23:31:33 -0300

    add

--- backend/src/Routes/routes.js
+++ backend/src/Routes/routes.js
@@ -4,9 +4,15 @@ const uploadConfig = require("../config/upload");
 
 const UserController = require("../app/controllers/UserController");
 const SpotController = require("../app/controllers/SpotController");
+const ProfileController = require("../app/controllers/ProfileController");
+const BookingController = require("../app/controllers/BookingController");
 
 const upload = multer(uploadConfig);
 
 routes.post("/user", UserController.store);
 
+routes.get("/spot", SpotController.index);
 routes.post("/spot", upload.single("thumbnail"), SpotController.store);
+routes.post("/spot/:id/bookings",  BookingController.store);
+
+routes.get("/profile", ProfileController.show);
